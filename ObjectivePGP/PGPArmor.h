ee38a7b268c3bf0ed5528be1f31366d51f7c448f
Dev Sanghani <dev_sanghani@yahoo.com>
2019-03-14 12:50:25 +0530

    Add class method to recognize armor type

--- ObjectivePGP/PGPArmor.h
+++ ObjectivePGP/PGPArmor.h
@@ -38,6 +38,8 @@ NS_SWIFT_NAME(Armor) @interface PGPArmor : NSObject
 
 + (BOOL)containsSignedMessageData:(NSData *)data;
 
++ (PGPArmorType)armorTypeForData:(NSData *)data;
+
 /// Helper function to convert input data (ASCII or binary) to array of PGP messages.
 + (nullable NSArray<NSData *> *)convertArmoredMessage2BinaryBlocksWhenNecessary:(NSData *)binOrArmorData error:(NSError * __autoreleasing _Nullable *)error;
 
